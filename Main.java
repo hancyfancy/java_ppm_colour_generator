f47fba9068bdf0ef57e6e958c156200412439e0b
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-10-07 16:07:10 +1100

    Remove option to configure opacity scale

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ColourGenerator colourGenerator = new ColourGenerator(9, "./colour/");
+        ColourGenerator colourGenerator = new ColourGenerator("./colour/");
         System.out.println(colourGenerator.getStorageLocation());
     }
 }
