5d18288e8e18ecc00c03a1431664b32611b8146f
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-10-07 16:05:37 +1100

    Remove option to configure opacity scale

--- PpmSchema.java
+++ PpmSchema.java
@@ -1,14 +1,12 @@
 public class PpmSchema extends AbstractPpm {
     private String magicNumber;
-    private int opacity;
     private String permutation;
-    public PpmSchema(int newOpacity) {
+    public PpmSchema() {
         super();
         this.setMagicNumber("P3");
-        this.setOpacity(newOpacity, getPixelSize());
     }
-    public PpmSchema(int newOpacity, String newPermutation) {
-        this(newOpacity);
+    public PpmSchema(String newPermutation) {
+        this();
         this.setPermutation(newPermutation);
     }
     public String getMagicNumber() {
@@ -17,14 +15,6 @@ public class PpmSchema extends AbstractPpm {
     private void setMagicNumber(String newMagicNumber) {
         if ((newMagicNumber != null) && (newMagicNumber != "")) {
             this.magicNumber = newMagicNumber;
-        }
-    }
-    public int getOpacity() {
-        return this.opacity;
-    }
-    private void setOpacity(int newOpacity, int pixelSize) {
-        if ((newOpacity > 0) && ((newOpacity % pixelSize) == 0) && (newOpacity <= getMaximumOpacity())) {
-            this.opacity = newOpacity;
         }
     }
     public String getPermutation() {
@@ -43,7 +33,7 @@ public class PpmSchema extends AbstractPpm {
     public String toString() {
         int pixelSize = getPixelSize();
         String pixelSizeString = String.valueOf(pixelSize);
-        String output = getMagicNumber().concat(" ").concat(pixelSizeString).concat(" ").concat(pixelSizeString).concat(" ").concat(String.valueOf(getOpacity())).concat(" ").concat(getPermutation().repeat(pixelSize*pixelSize).replaceAll(".(?=.)", "$0 ")).concat("\n");
+        String output = getMagicNumber().concat(" ").concat(pixelSizeString).concat(" ").concat(pixelSizeString).concat(" ").concat(String.valueOf(getMaximumOpacity())).concat(" ").concat(getPermutation().repeat(pixelSize*pixelSize).replaceAll(".(?=.)", "$0 ")).concat("\n");
         return output;
     }
 }
